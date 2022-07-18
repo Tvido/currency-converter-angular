42ef9635a9fac510ff8540e1c44ef8e3e01bfc62
Volodymyr Viter <mad.tvido@gmail.com>
2022-07-18 21:58:13 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+
+interface ICurrency {
+  [key: string]: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'currency-converter-angular';
-  amountFrom: number = + 0
-  amountTo?: number = + 0
-  currencyFrom?: string
-  currencyTo?: string
-  currencyList: any
-  result: any
+  amountFrom: number = 0;
+  amountTo: number = 0;
+  currencyFrom: string = 'UAH';
+  currencyTo: string = 'USD';
+  currencyList: ICurrency = {};
+  result: any;
 
-  constructor(public http: HttpClient) {
-  }
+  constructor(public http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get("https://cdn.cur.su/api/latest.json")
+    this.http
+      .get('https://cdn.cur.su/api/latest.json')
       .subscribe((response) => {
-        this.result = response
-        this.currencyList = Object.keys(this.result.rates)
-      })
+        this.result = response;
+        this.currencyList = this.result.rates;
+      });
   }
 
-  onChangeInputFrom(e: any): void {
-    this.amountFrom = e.target.value
-    console.log('amountFrom :>> ', this.amountFrom);
+  onChangeValue(e: InputEvent | Event) {
+    console.log('e', e);
+
+    switch ((e.target as HTMLInputElement).name) {
+      case 'amountFrom':
+      case 'currencyFrom':
+        this.amountTo = this.exchange(
+          this.amountFrom,
+          this.currencyFrom,
+          this.currencyTo
+        );
+        break;
+
+      case 'amountTo':
+      case 'currencyTo':
+        this.amountFrom = this.exchange(
+          this.amountTo,
+          this.currencyTo,
+          this.currencyFrom
+        );
+
+        break;
+
+      default:
+        break;
+    }
   }
 
-  onChangeInputTo(e: any) {
-    this.amountTo = e.target.value
-    console.log('amountTo :>> ', this.amountTo);
-  }
-
-  onChangeSelectFrom(e: any): void {
-    this.currencyFrom = e.target.value
-    console.log('currencyFrom :>> ', this.currencyFrom);
-
-  }
-
-  onChangeSelectTo(e: any): void {
-    this.currencyTo = e.target.value
-    console.log('currencyTo :>> ', this.currencyTo);
-
-  }
-
-  count(amountFrom: number = this.amountFrom) {}
-
-  getCurrentCurrency(currencyList = this.currencyList) {
-
+  exchange(qtt: number, fromCurrency: string, toCurrency: string) {
+    return (
+      (qtt * this.currencyList[toCurrency]) / this.currencyList[fromCurrency]
+    );
   }
 }
